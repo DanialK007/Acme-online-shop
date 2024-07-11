363ef10178b7e08f537e003cbf44822096cbeb9b
Danial <82217321+DanialK007@users.noreply.github.com>
2024-07-12 01:10:32 +0630

    v 1.0

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import { Ecommerce } from "@/components/component/ecommerce";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <Ecommerce />
+    </main>
+  );
+}
